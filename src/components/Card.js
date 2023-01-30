a31f8c86dffe45a512cae47612341d3684305107
Michael Dorst <m@mdorst.net>
2023-01-30 00:30:20 -0800

    Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from "react"
+import card_img from "../images/katie-zaferes.png"
+import star from "../images/star.png"
+import "./Card.css"
+
+export default function Card() {
+  return (
+    <div class="card">
+      <img src={card_img} alt="Katie Zaferes" />
+      <div class="description">
+        <div class="rating">
+          <img class="star" src={star} alt="Star" />
+          <p class="rating">5.0 (6)-USA</p>
+        </div>
+        <p class="description-text">Lessons with Katie Zaferes</p>
+        <div class="price">
+          <p class="bold">From $136&nbsp;</p>
+          <p>/ person</p>
+        </div>
+      </div>
+    </div>
+  )
+}
